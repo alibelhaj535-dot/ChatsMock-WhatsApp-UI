7364c3670bfd01c5202b746017d4f2e00e81693e
alibelhaj535-dot <alibelhaj535@gmail.com>
2026-04-22 02:59:55 +0100

    Create MainActivity.kt

---
+++ app/src/main/java/com/playfake/chatsmock/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.playfake.chatsmock
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.button_chat).setOnClickListener {
+            startActivity(Intent(this, ChatActivity::class.java))
+        }
+    }
+}
